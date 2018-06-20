8364ea00773ebfd48361ee00a89895c1f5eb6746
Yair Shlomo <yyaaiirr1@gmail.com>
2018-06-20 19:20:46 +0300

    fixprogerssbar

--- app/src/main/java/com/example/yair144/imagemobile/ImageService.java
+++ app/src/main/java/com/example/yair144/imagemobile/ImageService.java
@@ -1,6 +1,8 @@
 package com.example.yair144.imagemobile;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -9,14 +11,18 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
+
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -32,6 +38,7 @@ public class ImageService extends Service {
 
     private BroadcastReceiver onWifiConnect;
     private int count;
+   // private ProgressBar progressBar;
 
     public ImageService() {
         count = 0;
@@ -45,6 +52,8 @@ public class ImageService extends Service {
     }
 
     @Override
+    @RequiresApi(api = Build.VERSION_CODES.O)
+
     public void onCreate() {
         super.onCreate();
         final IntentFilter theFilter = new IntentFilter();
@@ -74,6 +83,7 @@ public class ImageService extends Service {
         Toast.makeText(this,"Service ending...", Toast.LENGTH_SHORT).show();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
 
     private void startTransfer() {
             new Thread(new Runnable() {
@@ -89,7 +99,7 @@ public class ImageService extends Service {
                         try {
                             //sends the message to the server
                             OutputStream output = socket.getOutputStream();
-                            sendImages(output);
+                           sendImages(output);
                            // output.close();
                         } catch (Exception e) {
                         } finally {
@@ -103,49 +113,74 @@ public class ImageService extends Service {
                 }
             }).start();
         }
-    private  void sendImages(OutputStream output) throws Exception {
-        File dcim = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM+"/Camera");
+        
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private  void sendImages(final OutputStream output) throws Exception {
+        File dcim = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/Camera");
         if (dcim == null) {
             return;
         }
+        final NotificationManager NM = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        final NotificationCompat.Builder builder;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel notificationChannel = new NotificationChannel("ImageServiceAppChannel", "Image Service App Channel", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            NM.createNotificationChannel(notificationChannel);
+            builder = new NotificationCompat.Builder(this,"ImageServiceAppChannel");
 
-        final NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        final int notify_id = 1;
+        } else {
+            builder = new NotificationCompat.Builder(this,"default");
+
+        }
+
+        final int notifyId = 1;
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setContentTitle("Passing images...");
+        builder.setContentText("Passing in progress...");
+        //Thread.sleep(10 * 1000);
+
         final File[] pics = dcim.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return (name.endsWith(".jpg") || name.endsWith(".jpeg") || name.endsWith(".png") ||
                         name.endsWith(".bmp") || name.endsWith(".gif"));
             }
         });
-        final int len = pics.length;
-
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (count < len) {
-                    builder.setProgress(len, count, false);
-                    builder.setContentText("Transfer in progress\n" + count + "/" + len + "Transferred");
-                    notificationManager.notify(notify_id, builder.build());
+                final int len = pics.length;
+                if (pics != null) {
+                    for (File pic : pics) {
+                        try {
+                            int barProgress = 0;
+
+                            count++;
+                            SendPhoto(output, pic);
+                            barProgress = barProgress + 100 / len;
+                            builder.setProgress(100, barProgress, false);
+                            NM.notify(notifyId, builder.build());
+                            Thread.sleep(2 * 1000);
+                        } catch (Exception e) {
+
+                        }
+                    }
                 }
-                builder.setProgress(len, count, false);
-                builder.setContentText("Transfer completed");
-                notificationManager.notify(notify_id, builder.build());
+                builder.setProgress(0, 0, false);
+                builder.setContentTitle("Finished transfer!");
+                builder.setContentText("Finished transfer!");
+                NM.notify(notifyId, builder.build());
             }
         }).start();
-
-        if (pics != null) {
-            for (File pic : pics) {
-                try {
-                    count++;
-                    SendPhoto(output, pic);
-                } catch (Exception e) {
-
-                }
-            }
-        }
-
+        ;
     }
+
+
+
+
 
     private byte[] getBytesFromBitmap(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
